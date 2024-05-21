d41aae8d54354076b96a2f48dbe011b0f4d57cf1
emurmudu <eliasmurmudu@gmail.com>
2024-05-21 17:50:54 +0600

    interface and schem model design done

---
+++ src/modules/products/product.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, model } from "mongoose";
+import { TInventory, TProduct, TVariant } from "./product.interface";
+  
+// variant sub schema
+  const variantSchema = new Schema<TVariant>({
+    type:  {type: String, required: true},
+    value: {type: String, required: true},
+  });
+
+
+// inventory sub schema
+const inventorySchema = new Schema<TInventory>({
+  quantity: { type: Number, default: 0 },
+  inStock: { type: Boolean, required: true },
+});
+
+// main schema
+const productSchema = new Schema<TProduct>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [variantSchema], required: true },
+  inventory: { type: inventorySchema, required: true }
+});
+
+// productSchema model
+export const Product = model<TProduct>('Product', productSchema);
+
